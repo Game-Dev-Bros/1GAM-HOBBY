deb18b3fdf6f75dcbbd346877432e62af600a026
Pedro Miguel Henriques Pereira <pmh.pereira@gmail.com>
2016-01-04 00:44:57 +0000

    Added basic player directional walking animations

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Animator animator;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    void ProcessInput()
+    {
+        bool up = false, down = false, left = false, right = false;
+
+        if(Input.GetButton("MoveUp"))
+        {
+            up = true;
+            down = left = right = false;
+        }
+
+        if(Input.GetButton("MoveDown"))
+        {
+            down = true;
+            up = left = right = false;
+        }
+
+        if(Input.GetButton("MoveLeft"))
+        {
+            left = true;
+            up = down = right = false;
+        }
+
+        if(Input.GetButton("MoveRight"))
+        {
+            right = true;
+            up = down = left = false;
+        }
+        
+        animator.SetBool("up", up);
+        animator.SetBool("down", down);
+        animator.SetBool("left", left);
+        animator.SetBool("right", right);
+    }
+}
