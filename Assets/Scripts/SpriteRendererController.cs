9babc2f7839ca01fad66e200ae2e843f01453a59
Pedro Miguel Henriques Pereira <pmh.pereira@gmail.com>
2016-01-07 13:34:53 +0000

    Optimized rendering settings for 5:4 aspect ratio

---
+++ Assets/Scripts/SpriteRendererController.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class SpriteRendererController : MonoBehaviour
+{
+    public int zOrder;
+    public bool fixedZOrder;
+    public int selectedOption;
+    public Material spriteMaterial;
+
+    public void RecalculateZOrder()
+    {
+        foreach (Transform child in transform)
+        {
+            if (!fixedZOrder)
+            {
+                zOrder = (int)-child.transform.position.y;
+            }
+
+            SpriteRenderer spriteRenderer = child.GetComponent<SpriteRenderer>();
+            if (spriteRenderer != null)
+            {
+                child.GetComponent<SpriteRenderer>().sortingOrder = zOrder;
+            }
+        }
+    }
+
+    public void UpdateMaterial()
+    {
+        foreach (Transform child in transform)
+        {
+            SpriteRenderer spriteRenderer = child.GetComponent<SpriteRenderer>();
+            if (spriteRenderer != null)
+            {
+                child.GetComponent<SpriteRenderer>().material = spriteMaterial;
+            }
+        }
+    }
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(SpriteRendererController))]
+public class SpriteOrderEditor : Editor
+{
+    private string[] zOptions = new string[] { "Dynamic", "Fixed" };
+
+    SpriteRendererController controller;
+
+    public override void OnInspectorGUI()
+    {
+        if (controller == null)
+        {
+            controller = (SpriteRendererController)target;
+        }
+
+        GUI.enabled = !Application.isPlaying;
+
+        controller.selectedOption = EditorGUILayout.Popup("Z-order Type", controller.selectedOption, zOptions);
+        controller.fixedZOrder = (controller.selectedOption == 1);
+
+        if (controller.fixedZOrder)
+        {
+            controller.zOrder = EditorGUILayout.IntField("Z-order Value", controller.zOrder);
+        }
+
+        if (GUILayout.Button("Recalculate Z-order"))
+        {
+            ((SpriteRendererController)target).RecalculateZOrder();
+        }
+
+        controller.spriteMaterial = (Material)EditorGUILayout.ObjectField("Sprite", controller.spriteMaterial, typeof(Material), false);
+
+        GUI.enabled = GUI.enabled && (controller.spriteMaterial != null);
+        if (GUILayout.Button("Update Material"))
+        {
+            ((SpriteRendererController)target).UpdateMaterial();
+        }
+    }
+}
+#endif
