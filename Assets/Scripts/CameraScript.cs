8cfef62327adc8a0fd62fe868c0e1986d9e255bc
Pedro Miguel Henriques Pereira <pmh.pereira@gmail.com>
2016-01-07 11:51:21 +0000

    Refactored CameraScript to update ortographicSize each frame

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -1,20 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraScript : MonoBehaviour {
-
-
-    private GameObject cam;
-    public int OrthographicSize = 8;
-
-	// Use this for initialization
-	void Start () {
-        cam = GameObject.FindGameObjectWithTag("MainCamera");
-        cam.GetComponent<Camera>().orthographicSize = OrthographicSize;
-	}
+public class CameraScript : MonoBehaviour
+{
+    public int orthographicSize = 8;
 	
-	// Update is called once per frame
-	void Update () {
-        cam.transform.position = new Vector3(transform.position.x, transform.position.y, cam.transform.position.z);
+	void Update()
+    {
+        Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z);
+        Camera.main.orthographicSize = orthographicSize;
 	}
 }
