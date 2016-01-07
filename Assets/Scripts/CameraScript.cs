cbd93bf7ea3ff03a9f0b0a37f9dddb9c9e59a308
Pedro Lucas <pmslucas@gmail.com>
2016-01-07 02:03:08 +0000

    New camera mode (Script component under player object)

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+
+    private GameObject cam;
+    public int OrthographicSize = 8;
+
+	// Use this for initialization
+	void Start () {
+        cam = GameObject.FindGameObjectWithTag("MainCamera");
+        cam.GetComponent<Camera>().orthographicSize = OrthographicSize;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        cam.transform.position = new Vector3(transform.position.x, transform.position.y, cam.transform.position.z);
+	}
+}
