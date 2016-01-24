6e1b928361c4e93de13be45021a992ebd9cea3eb
Pedro Lucas <pmslucas@gmail.com>
2016-01-24 20:38:33 +0000

    Added PersistentScript

---
+++ Assets/Scripts/PersistentScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PersistentScript : MonoBehaviour {
+
+    public GameObject prefab;
+
+	// Use this for initialization
+	void Awake () {
+        if (GameObject.Find("PersistentDataObject") == null)
+        {
+            var o = Instantiate(prefab);
+            o.name = "PersistentDataObject";
+        }
+	}
+
+}
