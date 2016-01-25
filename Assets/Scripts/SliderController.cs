920644d105803b2c76b6a5140fba568631ca8bdb
Pedro Miguel Henriques Pereira <pmh.pereira@gmail.com>
2016-01-25 19:07:06 +0000

    Decay pointer based on inclination direction

--- Assets/Scripts/SliderController.cs
+++ Assets/Scripts/SliderController.cs
@@ -3,37 +3,43 @@
 public class SliderController : MonoBehaviour
 {
     public GameObject pointer;
+    public float moveSpeed;
     public float minValue, maxValue;
     public float minAngle, maxAngle;
     public bool invertRotation;
     public float value;
 
+    private ClockManager clock;
+
     void Awake()
     {
         value = PlayerPrefs.GetFloat("Pointer");
+        clock = GameObject.FindObjectOfType<ClockManager>();
         UpdateSlider(value);
     }
 
     void Update()
     {
-        //value += 0.05f;
-        //UpdateSlider(value);
+        float distanceToMiddle = (value - (maxValue - minValue) / 2);
+        int moveDirection = distanceToMiddle > 0 ? 1 : -1;
+        
+        value += moveSpeed * moveDirection * Time.deltaTime;
+        UpdateSlider(value);
     }
 
     public void UpdateSlider(float value = 0)
     {
-
         this.value = Mathf.Min(Mathf.Max(value, minValue), maxValue);
+
+        float angle = (maxAngle - minAngle) * (value / (maxValue - minValue)) + minAngle;
+
+        if(invertRotation)
+        {
+            angle = -angle;
+        }
 
         if(pointer != null)
         {
-            float angle = (maxAngle - minAngle) * (value / (maxValue - minValue)) + minAngle;
-
-            if(invertRotation)
-            {
-                angle = -angle;
-            }
-
             pointer.transform.eulerAngles = new Vector3(0, 0, angle);
         }
     }
